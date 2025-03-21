5ec0dcba5c8961ed58933fa7b799b951beaabadd
sushmanthvaka <vakasushmanth@gmail.com>
2025-03-21 14:59:54 +0530

    initial commit

--- sushmath.py
+++ sushmath.py
@@ -1,8 +1,8 @@
 # Simple Python code to greet the user based on their name and age
 
 # Ask for user input
-name = input("Wherw are u from sushmamth ? ")
-age = int(input("what you are doing currently "))
+name = input("Wherw are u from ushmamth ? ")
+age = int(input("what you are doingnow & currently "))
 
 # Print a personalized greeting
 print(f"Hello u are from  {name}, and you are {age}, years old.")
